99d7e7a1d839e5bbb658777776fb8d0b8f8a41b4
Sébastien Cartoux <cartoux.sebastien@orange.fr>
2023-01-18 00:13:00 +0100

    Fixed default language

--- src/translate.php
+++ src/translate.php
@@ -4,4 +4,4 @@ bindtextdomain('main', __DIR__ . '/../locale/');
 bind_textdomain_codeset('main', 'UTF-8');
 textdomain('main');
 
-setlocale(LC_ALL, $_COOKIE['locale'] . '.UTF-8');
+setlocale(LC_ALL, $_COOKIE['locale'] ?? 'en_GB' . '.UTF-8');
